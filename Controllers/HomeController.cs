f8908389fd5b0fc0efbf8c49cda52ab3b3f28873
skoley1 <sudkoley@in.ibm.com>
2019-12-27 18:32:53 +0530

    3rd commit

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -16,7 +16,10 @@ namespace locationfinder.Controllers
             {
                 new Locations(1, "IBM DLF1","IBM DLF1, Kolkata", 22.581431, 88.450462),
                 new Locations(2, "IBM BCS","IBM BCS, Kolkata", 22.5718, 88.4346),
-                new Locations(3, "Bengaluru","Bengaluru, Karnataka", 12.972442, 77.580643)
+                new Locations(3, "IBM OZONE 3","IBM OZONE 3, Pune", 18.4908, 73.9524),
+                new Locations(4, "IBM Hinjawadi","IBM Hinjawadi, Pune", 18.6057, 73.7159),
+                new Locations(5, "IBM Bangalore :: Manyata Embassy Business Park","IBM Bangalore :: Manyata Embassy Business Park, Bangalore", 18.6057, 73.7159),
+                new Locations(5, "IBM Chennai","IBM Chennai, Chennai", 13.023099, 80.176303)
             };
             model.LocationList = locations;
             return View(model);
